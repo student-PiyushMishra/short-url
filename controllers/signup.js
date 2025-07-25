b42ce35de55743b6837999616113e8c8012d0561
Piyush Mishra <student.piyushmishra@gmail.com>
2025-07-26 02:25:58 +0530

    solved the problem of duplicate email

--- controllers/signup.js
+++ controllers/signup.js
@@ -2,11 +2,13 @@ import usersModel from "../models/user.js"
 
 async function makeAUser(req,res,next){
 	if(req.body.fullName, req.body.email, req.body.password){
-          await usersModel.create({
-		fullName: req.body.fullName,
-		email: req.body.email,
-		password: req.body.password
-	  })
+    const user = await usersModel.find({email: req.body.email})
+    if(user.length!=0){res.render('info',{info:"Error: Duplicate Email",msg:"This email already exists, if yours kindly go to login (click me)...",href:"/auth/login"})}
+    await usersModel.create({
+      fullName: req.body.fullName,
+      email: req.body.email,
+      password: req.body.password
+    })
     next();
 	}
  	return null;
