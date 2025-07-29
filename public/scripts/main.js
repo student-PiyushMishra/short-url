b21e2c6ac058089d95eb75757546c80ec3786457
Piyush Mishra <student.piyushmishra@gmail.com>
2025-07-29 22:33:21 +0530

    mended the indentation

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,8 @@
-    data = data.reverse()
-    const tableBody = document.getElementById("linkTableBody");
-    data.forEach(link => {
-      const tableRow = document.createElement('tr')
-      const shortId = document.createElement('td')
-      const redirectUrl = document.createElement('td')
-      shortId.textContent = link.shortId 
-      redirectUrl.textContent =  link.redirectUrl  
-      tableRow.appendChild(shortId) 
-      tableRow.appendChild(redirectUrl) 
-      tableBody.appendChild(tableRow)
-    })
+data = data.reverse()
+let arrayIndex = array.length
+while (arrayIndex--) {
+  
+}
+data.forEach(link => {
+  
+})
