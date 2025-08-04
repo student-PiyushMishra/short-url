0aa80468bf9d3007f1be34846780e8854554d296
Piyush Mishra <student.piyushmishra@gmail.com>
2025-08-04 15:21:01 +0530

    route to ping

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-import express from "express"
-import urlRoute from "./routes/url.js" 
-import dotenv from "dotenv"
-import ejs from "ejs"
-import connectToMongoDB from "./connect.js"
-import path from "path"
-import { fileURLToPath } from 'url';
-import redirectURL from "./routes/redirect.js"
-import authRoutes from "./routes/auth.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import urlRoute from "./routes/url.js";
+import dotenv from "dotenv";
+import ejs from "ejs";
+import connectToMongoDB from "./connect.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import redirectURL from "./routes/redirect.js";
+import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 2000;
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename) 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.set("view engine","ejs")
-app.use(express.static(path.join(__dirname,"public")))
-app.use(cookieParser())
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-connectToMongoDB(process.env.CONNECTION_STRING).then(()=>{
-	console.log("MONGODB Connected Successfully!")
-})
-
-app.use((req,res,next)=>{
-  if(!JSON.parse(process.env.availableBaseRoutes).includes(req.url.split("/")[1])){
-    if(req.url.split('/').length > 2){
-      res.render('404')
-    }
-  }
-  next()
-})
-
-app.get('/sitemap.xml', (req, res) => {
-  res.set('Content-Type', 'application/xml');
-  res.sendFile(path.join(__dirname, 'public', 'sitemap.xml'));
+connectToMongoDB(process.env.CONNECTION_STRING).then(() => {
+  console.log("MONGODB Connected Successfully!");
 });
 
+app.use((req, res, next) => {
+  if (
+    !JSON.parse(process.env.availableBaseRoutes).includes(req.url.split("/")[1])
+  ) {
+    if (req.url.split("/").length > 2) {
+      res.render("404");
+    }
+  }
+  next();
+});
 
-app.use('/url',urlRoute)
-app.use("/auth",authRoutes)
-app.use("/",redirectURL)
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
 
-app.listen(port,()=>{
-	console.log(`App is running on port: ${port}`)
-})
+app.get("/sitemap.xml", (req, res) => {
+  res.set("Content-Type", "application/xml");
+  res.sendFile(path.join(__dirname, "public", "sitemap.xml"));
+});
+
+app.use("/url", urlRoute);
+app.use("/auth", authRoutes);
+app.use("/", redirectURL);
+
+app.listen(port, () => {
+  console.log(`App is running on port: ${port}`);
+});
